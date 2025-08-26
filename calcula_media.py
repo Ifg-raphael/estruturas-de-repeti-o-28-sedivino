42d6b4d459fb852513d07c1557fa67383c7a6072
Eduardo Divino <eduardo.divino@discente.ufg.br>
2025-08-26 16:03:31 -0300

    lista4_ex28_edu

--- calcula_media.py
+++ calcula_media.py
@@ -1,1 +1,56 @@
-# Sua solução aqui
+'''
+## Lista 04 - Exercício 28
+
+Faça um programa para entrar com 5 números e imprimir a média desses números.
+
+O programa deve assumir que a entrada e saída seja exatamente no formato dado nos exemplos a seguir.
+**Não adicione outras mensagens ou mude a capitalização das letras pois se fizer isso o teste não passará!**
+
+---
+
+**Exemplo 01:**
+
+Entrada:
+```
+1 2 3 4 5
+```
+Saída:
+```
+3.00
+```
+
+---
+
+**Exemplo 02:**
+
+Entrada:
+```
+-10 -20 -30 -40 -50
+```
+Saída:
+```
+-30.00
+```
+
+---
+
+**Exemplo 03:**
+
+Entrada:
+```
+4 4 4 4 4
+```
+Saída:
+```
+4.00
+```
+
+'''
+# Entrada de dados
+numeros = list(map(float, input().split()))
+
+# Cálculo da média
+media = sum(numeros) / len(numeros)
+
+# Saída formatada
+print(f"{media:.2f}")
